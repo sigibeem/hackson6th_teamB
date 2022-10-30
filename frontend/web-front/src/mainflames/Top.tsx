9a93b7e2f79d93c98a506fe5e4b2c8b876cf36cc
nto300002 <samonkntd@gmail.com>
2022-10-30 11:17:51 +0900

    add: Top画面のrouteing設定

--- frontend/web-front/src/mainflames/Top.tsx
+++ frontend/web-front/src/mainflames/Top.tsx
@@ -1,10 +1,33 @@
 import React from 'react';
 import '../styles/App.css';
+import { useNavigate } from "react-router-dom";
 
 function Top() {
+  const navigate = useNavigate();
+  const handleClickWeaponList = () => {
+    navigate('/weaponlist')
+  }
+  const handleClickHead = () => {
+    navigate('/head')
+  }
+  const handleClickArmor = () => {
+    navigate('/armor')
+  }
+  const handleClickShoes = () => {
+    navigate('/shoes')
+  }
   return (
     <div className="Top">
-      top
+      <ul className='btnList'>
+        <li className='btn' onClick={handleClickWeaponList}>ブキ</li>
+        <li className='btn' onClick={handleClickHead}>アタマ</li>
+        <li className='btn' onClick={handleClickArmor}>フク</li>
+        <li className='btn' onClick={handleClickShoes}>クツ</li>       
+      </ul>
+      <div className='hukidashi'>
+        <p>キミの知りたい<br /><span className='textEmphasis'>アイテム</span>を教えて<br />ほしいデシ！</p>
+      </div>           
+      <p className='bukichi'></p>  
     </div>
   );
 }
