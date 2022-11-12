623055acb4347d9f75b5eb64f342f9138cd6d5c5
nto300002 <samonkntd@gmail.com>
2022-11-12 23:14:14 +0900

    add: totallingPageComponents

---
+++ frontend/web-front/src/components/TotallingPageComponents/SelectPlayModeWeapon2.tsx
@@ -0,0 +1,35 @@
+import { useNavigate } from 'react-router-dom'
+import '../../styles/listpage/listitems.css'
+import '../../styles/menulist.css'
+
+const SelectPlayModeWeapon2 = () => {
+  const navigate = useNavigate()
+  const handleNavigateTotallingNawabari = () =>{
+    navigate("/totalling/result", {state: 'ナワバリバトル'})
+  }
+  const handleNavigateTotallingHero = () =>{
+    navigate("/totalling/result", {state: 'ヒーローモード'})
+  }
+  const handleNavigateTotallingSamon = () =>{
+    navigate("/totalling/result", {state: 'サーモンラン'})
+  }
+  return (
+    <div>
+      <ul className='btnList'>
+        <li className='listStyle'>
+          <button className='btn' >ブキ2</button>
+        </li>
+      </ul>
+      <h1 className='h1'>どれをプレイした？</h1>
+      <ul className='listitemsUlRight'>
+        <li className='listitemsLiRight' onClick={handleNavigateTotallingNawabari}>ナワバリバトル</li>
+        <br />
+        <li className='listitemsLiRight' onClick={handleNavigateTotallingHero}>ヒーローモード</li>
+        <br />
+        <li className='listitemsLiRight' onClick={handleNavigateTotallingSamon}>サーモンラン</li>
+      </ul>
+    </div>
+  )
+}
+
+export default SelectPlayModeWeapon2
--- frontend/web-front/src/components/TotallingPageComponents/VictoryOrDefeat.tsx
+++ frontend/web-front/src/components/TotallingPageComponents/VictoryOrDefeat.tsx
@@ -1,16 +1,39 @@
-import { Link } from 'react-router-dom'
-import '../../styles/listpage/listitems.css'
+import { useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
+import axios, { AxiosInstance } from 'axios'
 import '../../styles/menulist.css'
+import '../../styles/index.css'
 
 const VictoryOrDefeat = () => {
+  const location = useLocation()
+  const [selectPlayMode] = useState(location.state)
+  const postAPIData = async (e: any) => {
+    // let instance: AxiosInstance
+
+    // instance = axios.create({
+    //     baseURL: 'http://localhost:8080',
+    // })
+    const datanum = e.currentTarget.dataset.num
+    axios.post("http://localhost:8080/api/vote", {"gamemode":selectPlayMode, "result":datanum})
+    .then(response=>{console.log("body:",response.data)})
+}
+
   return (
     <div>
-      <Link to='/' style={{ textDecoration: 'none' }}>
-        <ul className='listitemsUlLeft'>
-          <li className='listitemsLiLeft'>勝ち</li>
-          <li className='listitemsLiLeft'>負け</li>
-        </ul>
-      </Link>
+      <h1 className='h1'>{selectPlayMode}</h1>
+      {/* <Link to='/' style={{ textDecoration: 'none' }}> */}
+          <ul className='resultUl'>
+            <li style={{ listStyle: 'none' }}>
+              <button data-num="0" onClick={postAPIData} className='resultLiWin'>勝ち</button>
+            </li>
+            <li style={{ listStyle: 'none' }}>
+              <button data-num="1" onClick={postAPIData} className='resultLiLose'>負け</button>
+            </li>
+          </ul>
+          {/* <li onClick={postAPIData} className='listitemsLiLeft'>勝ち</li>
+          <li onClick={postAPIData} className='listitemsLiLeft'>負け</li> */}
+      
+      {/* </Link> */}
     </div>
   )
 }
