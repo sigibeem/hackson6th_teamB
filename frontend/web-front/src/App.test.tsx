86f35b787d6e2b5a511ed4af2ab7493da5630dbe
nto300002 <samonkntd@gmail.com>
2022-11-13 11:36:29 +0900

    add: mainflames Top.tsx test

--- frontend/web-front/src/App.test.tsx
+++ frontend/web-front/src/App.test.tsx
@@ -1,9 +1,56 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './mainflames/Top';
+import userEvent from '@testing-library/user-event';
+import Top from './mainflames/Top';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// Navigatorモック準備
+const mockedNavigator = jest.fn();
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockedNavigator,
+}));
+
+describe('useNavigate', () => {
+  test('ホーム画面に遷移すること', () => {
+    // / を引数にNavigatorが呼び出されること
+    Object.defineProperty(window, 'location', {
+      value: {
+        pathname: '/',
+      },
+    });
+    
+  });
+  test('ブキリスト画面に遷移すること', async () => {
+    render(
+      <Top />
+    )
+    userEvent.click(screen.getByText('ブキ'));
+    // /weaponsを引数にNavigatorが呼び出されること
+    expect(mockedNavigator).toHaveBeenCalledWith('/weapons');
+  });
+  test('アタマ画面に遷移すること', () => {
+    render(
+      <Top />
+    )
+    userEvent.click(screen.getByText('アタマ'));
+    // /headを引数にNavigatorが呼び出されること
+    expect(mockedNavigator).toHaveBeenCalledWith('/head');
+  });
+  test('フク画面に遷移すること', () => {
+    render(
+      <Top />
+    )
+    userEvent.click(screen.getByText('フク'));
+    // /armorを引数にNavigatorが呼び出されること
+    expect(mockedNavigator).toHaveBeenCalledWith('/armor');
+  });
+  test('クツ画面に遷移すること', () => {
+    render(
+      <Top />
+    )
+    userEvent.click(screen.getByText('クツ'));
+    // /shoesを引数にNavigatorが呼び出されること
+    expect(mockedNavigator).toHaveBeenCalledWith('/shoes');
+  });
 });
+
+
