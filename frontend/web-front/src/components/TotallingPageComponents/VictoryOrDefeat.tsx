d1d1e99d9b3a879808a590fbfefee4943cac493f
nto300002 <samonkntd@gmail.com>
2022-11-23 12:01:10 +0900

    fix: result 修正

--- frontend/web-front/src/components/TotallingPageComponents/VictoryOrDefeat.tsx
+++ frontend/web-front/src/components/TotallingPageComponents/VictoryOrDefeat.tsx
@@ -3,16 +3,13 @@ import { Link, useLocation } from 'react-router-dom'
 import axios from 'axios'
 import '../../styles/menulist.css'
 import '../../styles/index.css'
+import blueInk from '../../images/background/blueInk1.png'
+import yellowInk from '../../images/background/yellowInk1.png'
 
 const VictoryOrDefeat = () => {
   const location = useLocation()
   const [selectPlayMode] = useState(location.state)
   const postAPIData = async (e: any) => {
-    // let instance: AxiosInstance
-
-    // instance = axios.create({
-    //     baseURL: 'http://localhost:8080',
-    // })
     const datanum = e.currentTarget.dataset.num
     axios.post("http://localhost:8080/api/vote", {"battle_mode":selectPlayMode, "result":datanum})
     .then(response=>{console.log("body:",response.data)})
